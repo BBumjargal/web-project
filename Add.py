dc3ec241b146ff9c46f17a0707c1897f1995dfce
Bumjargal <bumjargal@Bumjargals-MacBook-Air.local>
2024-02-21 15:05:54 +0800

    Add Add.py first time

---
+++ Add.py
@@ -0,0 +1,12 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+niilber = 0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count += 1
+    niilber += float(line[20:].rstrip())
+print(niilber/count)
+print("Done")
